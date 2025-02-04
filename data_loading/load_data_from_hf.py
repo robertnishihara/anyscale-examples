2ed79f77bec61d55cbe6143d6d65bba2625a0b44
Robert Nishihara <rkn@anyscale.com>
2025-02-04 00:38:47 -0800

    Add more datasets

--- data_loading/load_data_from_hf.py
+++ data_loading/load_data_from_hf.py
@@ -7,35 +7,33 @@ from huggingface_hub import HfFileSystem, HfApi, DatasetCardData, errors
 
 
 dataset_infos = {
-    "HuggingFaceFV/finevideo": (None, "train", "parquet"),
-    "HuggingFaceFW/fineweb-2": (None, "train", "parquet"),
-
-    # BELOW ARE ALL UNTESTED
-
-    "beans": ("default", "train", "parquet"),
-    "glue": ("mrpc", "train", "parquet"),
-    "nvidia/HelpSteer2": ("default", "train", "parquet"),
-    "fka/awesome-chatgpt-prompts": ("default", "train", "parquet"),
-    "LLM360/TxT360": ("default", "train", None),  # Doesn't support Parquet
-    "KingNish/reasoning-base-20k": ("default", "train", "parquet"),
-    "Zyphra/Zyda-2": ("default", "train", "parquet"),  # Fails with job manager crashed???
-    "Marqo/marqo-GS-10M": ("default", "in_domain", "parquet"),
-    "BAAI/IndustryCorpus2": ("default", "in_domain", "parquet"),  # Timeout?
-    "mozilla-foundation/common_voice_17_0": ("ab", "train", "parquet"),  # Permissions
-    "rombodawg/Everything_Instruct_Multilingual": ("default", "train", "parquet"),
-    "facebook/multilingual_librispeech": ("dutch", "train", "parquet"),
-    "databricks/databricks-dolly-15k": ("default", "train", "parquet"),
-    "HuggingFaceTB/cosmopedia": ("auto_math_text", "train", "parquet"),
-    "HuggingFaceTB/smollm-corpus": ("cosmopedia-v2", "train", "parquet"),  # aiohttp.client_exceptions.ClientResponseError: 400, message='Bad Request', url=URL('https://huggingface.co/api/datasets/HuggingFaceTB/smollm-corpus/parquet/cosmopedia-v2/train/20.parquet')
-    "HuggingFaceFW/fineweb": ("default", "train", "parquet"),  # Error: {'error': 'The computed response content exceeds the supported size in bytes (10000000).'}
-    "ylecun/mnist": ("default", "train", "parquet"),  # Exception: Dataset generation failed: {'error': 'Not found.'}
-    "wikimedia/wikipedia": ("20231101.ab", "train", "parquet"),
-    "HuggingFaceM4/OBELICS": ("default", "train", "parquet"),  # aiohttp.client_exceptions.ClientResponseError: 429, message='Too Many Requests', url=URL('https://huggingface.co/api/datasets/HuggingFaceM4/OBELICS/parquet/default/train/763.parquet')
+    # Parquet
+    "HuggingFaceFV/finevideo": ("data", "train", "parquet"),
+    "HuggingFaceFW/fineweb-2": ("data", "train", "parquet"),
+    "wikimedia/wikipedia": (None, "train", "parquet"),
     "nvidia/OpenMathInstruct-2": (None, "train", "parquet"),
-    "allenai/c4": ("en", "train", "parquet"),
-    "PolyAI/minds14": ("en-US", "train", "parquet"),  # Doesn't support Parquet, weird directory structure
+    "HuggingFaceFW/fineweb": ("data", "train", "parquet"),
+    "HuggingFaceTB/cosmopedia": ("data", "train", "parquet"),
+    "BAAI/IndustryCorpus2": (None, "train", "parquet"),
+    "HuggingFaceTB/smollm-corpus": (None, "train", "parquet"),
+    "ylecun/mnist": (None, "train", "parquet"),
+    "beans": (None, "train", "parquet"),
+    "glue": (None, "train", "parquet"),
+    "Zyphra/Zyda-2": ("data", "train", "parquet"),
+    "Marqo/marqo-GS-10M": ("data", "in_domain", "parquet"),
+    "HuggingFaceM4/OBELICS": ("data", "train", "parquet"),
     "rotten_tomatoes": (None, "train", "parquet"),
-    "teknium/OpenHermes-2.5": (None, "train", "parquet"),  # Some issue with reading json
+
+    # Binary
+    "mozilla-foundation/common_voice_17_0": ("audio", "train", "binary"),  # .tar
+    "PolyAI/minds14": ("data", "train", "binary"),  # .zip
+    "facebook/multilingual_librispeech": ("data", "train", "binary"),  # .tar.gz
+
+    # Json
+    "allenai/c4": (None, "train", "json"),  # .json.gz
+    "teknium/OpenHermes-2.5": (None, "train", "json"),  # .json
+    "rombodawg/Everything_Instruct_Multilingual": (None, "train", "json"),  # .json
+    "databricks/databricks-dolly-15k": (None, "train", "json"),  # .jsonl
 }
 
 dataset_name = os.environ.get("DATASET_NAME")
@@ -52,39 +50,20 @@ FILE_LIMIT = None  # None for unlimited, e.g., 2 for a small value
 # ray.data.DataContext.get_current().retried_io_errors.append("429 Client Error: Too Many Requests for url")
 
 
-def retrieve_hf_data_files(dataset_name, split=None, revision=None, data_dir=None, data_files=None, token=None):
-    hfh_dataset_info = HfApi("https://huggingface.co").dataset_info(
-        dataset_name,
-        revision=revision,
-        token=token,
-        # timeout=100.0,
-    )
-
-    revision = hfh_dataset_info.sha  # fix the revision in case there are new commits in the meantime
-    base_path = f"hf://datasets/{dataset_name}@{revision}/{data_dir or ''}".rstrip("/")
-
-    dataset_card_data = DatasetCardData()
-    metadata_configs = datasets.utils.metadata.MetadataConfigs.from_dataset_card_data(dataset_card_data)
-
-    # we need a set of data files to find which dataset builder to use
-    # because we need to infer module name by files extensions
-    if data_files is not None:
-        patterns = datasets.data_files.sanitize_patterns(data_files)
-    elif metadata_configs and not data_dir and "data_files" in next(iter(metadata_configs.values())):
-        patterns = datasets.data_files.sanitize_patterns(next(iter(metadata_configs.values()))["data_files"])
-    else:
-        patterns = datasets.data_files.get_data_patterns(base_path) #, download_config=self.download_config)
+def retrieve_hf_data_files(dataset_name, data_dir=None, token=None):
+    # If you are unsure what the data_dir should be, run something like
+    #     HfFileSystem(token=token).ls(base_path)
+    base_path = f"hf://datasets/{dataset_name}/{data_dir or ''}".rstrip("/")
+    patterns = datasets.data_files.get_data_patterns(base_path)
     data_files = datasets.data_files.DataFilesDict.from_patterns(
         patterns,
         base_path=base_path,
         allowed_extensions=datasets.load.ALL_ALLOWED_EXTENSIONS,
-        # download_config=self.download_config,
     )
-    return data_files[split]
+    return data_files
 
 
-data_files = retrieve_hf_data_files(dataset_name, data_dir=data_dir, split=split)
-
+data_files = retrieve_hf_data_files(dataset_name, data_dir=data_dir)[split]
 
 ds = ray.data.read_parquet(
     data_files[:FILE_LIMIT],
